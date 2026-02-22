14194bed4602b9f111c5b6ba4cb32c1634b3af50
unf0r9et <unforgettableunf2006@gmail.com>
2026-02-23 02:29:04 +0300

    feat: Added abstract class <Shape>

---
+++ LAB1/src/main/java/shape/Shape.java
@@ -0,0 +1,12 @@
+package shape;
+
+public abstract class Shape {
+    protected int x, y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void draw();
+}
