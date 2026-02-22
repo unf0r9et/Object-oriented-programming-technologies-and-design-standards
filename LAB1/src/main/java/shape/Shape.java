0cb0f1e6e48e09b9335acf682d21dbd1f15f938e
unf0r9et <unforgettableunf2006@gmail.com>
2026-02-23 02:41:35 +0300

    feat: Added color in abstract class <Shape>

--- LAB1/src/main/java/shape/Shape.java
+++ LAB1/src/main/java/shape/Shape.java
@@ -1,12 +1,16 @@
 package shape;
 
+
+import java.awt.*;
+
 public abstract class Shape {
     protected int x, y;
+    protected Color color;
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public abstract void draw();
+    public abstract void draw(Graphics g);
 }
