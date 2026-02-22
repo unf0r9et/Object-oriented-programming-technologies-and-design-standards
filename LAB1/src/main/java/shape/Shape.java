451410c53799253c882d234fad3bf4cd38749813
unf0r9et <unforgettableunf2006@gmail.com>
2026-02-23 02:46:57 +0300

    Added color to constructor

--- LAB1/src/main/java/shape/Shape.java
+++ LAB1/src/main/java/shape/Shape.java
@@ -7,9 +7,10 @@ public abstract class Shape {
     protected int x, y;
     protected Color color;
 
-    public Shape(int x, int y) {
+    public Shape(int x, int y, Color color) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 
     public abstract void draw(Graphics g);
