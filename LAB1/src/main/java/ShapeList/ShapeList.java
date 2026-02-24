f99c2aa63384fd6dba6714a973c7499eb162b6eb
unf0r9et <unforgettableunf2006@gmail.com>
2026-02-25 01:32:10 +0300

    fear: Added menu like <POINT> and new rendering logic

--- LAB1/src/main/java/ShapeList/ShapeList.java
+++ LAB1/src/main/java/ShapeList/ShapeList.java
@@ -1,24 +1,120 @@
 package ShapeList;
 
+import shape.*;
+import shape.Point;
+import shape.Rectangle;
 import shape.Shape;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
 public class ShapeList extends JPanel {
-    private ArrayList<Shape> shapeList = new ArrayList<>();
 
-    public void addShape(Shape sp) {
-        shapeList.add(sp);
+    public enum Shapes {Circle, Ellipse, Line, Point, Rectangle, Square, Triangle}
+
+    private ArrayList<Shape> shapes = new ArrayList<>();
+
+    private Shape currentShape;
+    private int startX, startY;
+
+
+    private Shapes selectedTool = Shapes.Ellipse;
+
+    public ShapeList() {
+
+        setBackground(Color.WHITE);
+
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startX = e.getX();
+                startY = e.getY();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (currentShape != null) {
+                    shapes.add(currentShape);
+                    currentShape = null;
+                    repaint();
+                }
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+                int currentX = e.getX();
+                int currentY = e.getY();
+
+                int x = Math.min(startX, currentX);
+                int y = Math.min(startY, currentY);
+
+                int width = Math.abs(currentX - startX);
+                int height = Math.abs(currentY - startY);
+
+
+                switch (selectedTool) {
+                    case Shapes.Circle:
+                        currentShape = new Circle(x, y, height, Color.BLUE);
+                        break;
+                    case Shapes.Ellipse:
+                        currentShape = new Ellipse(x, y, width, height, Color.BLUE);
+                        break;
+                    case Shapes.Line:
+                        currentShape = new Line(startX, startY, currentX, currentY, Color.BLUE);
+                        break;
+                    case Shapes.Point:
+                        currentShape = new Point(x, y, Color.BLUE);
+                        break;
+                    case Shapes.Rectangle:
+                        currentShape = new Rectangle(x, y, width, height, Color.BLUE);
+                        break;
+                    case Shapes.Square:
+                        currentShape = new Square(x, y, width, Color.BLUE);
+                        break;
+                    case Shapes.Triangle:
+
+                        int baseHalf = Math.abs(currentX - startX);
+
+                        int leftX = startX - baseHalf;
+
+                        int rightX = startX + baseHalf;
+
+                        currentShape = new Triangle(x, y, leftX, currentY, rightX, currentY, Color.BLUE);
+                        break;
+                }
+
+                repaint();
+            }
+        });
+    }
+
+    public void setTool(Shapes tool) {
+        this.selectedTool = tool;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for (Shape s : shapeList) {
+        for (Shape s : shapes) {
             s.draw(g);
         }
+
+        if (currentShape != null) {
+            currentShape.draw(g);
+        }
+    }
+
+    public void clearCanvas() {
+        shapes.clear();
+        currentShape = null;
+        repaint();
     }
 }
--- LAB1/src/main/java/org/example/Main.java
+++ LAB1/src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
 package org.example;
 
-import javax.swing.JFrame;
-import java.awt.Color;
-
 import ShapeList.ShapeList;
-import shape.*;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,21 +12,42 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         ShapeList canvas = new ShapeList();
-        canvas.setBackground(Color.BLACK);
 
-        canvas.addShape(new Ellipse(100, 90, 200, 500, Color.ORANGE));
-        canvas.addShape(new Ellipse(110, 100, 180, 480, Color.ORANGE));
-        canvas.addShape(new Ellipse(1000, 90, 200, 500, Color.BLUE));
-        canvas.addShape(new Ellipse(1010, 100, 180, 480, Color.BLUE));
-        canvas.addShape(new Circle(220, 400, 40, Color.RED));
-        canvas.addShape(new Square(320, 330, 50, Color.GREEN));
-        canvas.addShape(new Triangle(420, 300, 470, 310, 520, 360, Color.YELLOW));
-        canvas.addShape(new Rectangle(560, 340, 80, 40, Color.PINK));
-        canvas.addShape(new Ellipse(720, 300, 80, 40, Color.green));
-        canvas.addShape(new Square(840, 250, 60, Color.MAGENTA));
-        canvas.addShape(new Circle(960, 360, 45, Color.ORANGE));
+        JPanel toolbar = new JPanel();
 
-        frame.add(canvas);
+        JButton circleBtn = new JButton("Circle");
+        JButton ellipseBtn = new JButton("Ellipse");
+        JButton lineBtn = new JButton("Line");
+        JButton pointBtn = new JButton("Point");
+        JButton rectBtn = new JButton("Rectangle");
+        JButton squareBtn = new JButton("Square");
+        JButton triangleBtn = new JButton("Triangle");
+
+        JButton clearBtn = new JButton("CLEAR");
+
+        circleBtn.addActionListener(e -> canvas.setTool(ShapeList.Shapes.Circle));
+        ellipseBtn.addActionListener(e ->  canvas.setTool(ShapeList.Shapes.Ellipse));
+        lineBtn.addActionListener(e ->  canvas.setTool(ShapeList.Shapes.Line));
+        pointBtn.addActionListener(e ->  canvas.setTool(ShapeList.Shapes.Point));
+        rectBtn.addActionListener(e -> canvas.setTool(ShapeList.Shapes.Rectangle));
+        squareBtn.addActionListener(e -> canvas.setTool(ShapeList.Shapes.Square));
+        triangleBtn.addActionListener(e -> canvas.setTool(ShapeList.Shapes.Triangle));
+
+        clearBtn.addActionListener(e -> canvas.clearCanvas());
+
+
+        toolbar.add(circleBtn);
+        toolbar.add(ellipseBtn);
+        toolbar.add(lineBtn);
+        toolbar.add(pointBtn);
+        toolbar.add(rectBtn);
+        toolbar.add(squareBtn);
+        toolbar.add(triangleBtn);
+
+        frame.add(toolbar, BorderLayout.NORTH);
+        frame.add(canvas, BorderLayout.CENTER);
+        frame.add(clearBtn, BorderLayout.SOUTH);
+
         frame.setVisible(true);
     }
 }
